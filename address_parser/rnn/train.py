669c54785df71395a1f4965afeb03ffb533ef518
Mohamed Abdelbary <mohamed.abdelbary@duedil.com>
2021-04-06 10:09:57 +0100

    increase epochs, reduce seq length and compute accuracy

--- address_parser/rnn/train.py
+++ address_parser/rnn/train.py
@@ -10,19 +10,28 @@ from address_parser.paf.util import chunks_from_iter, csv_records_to_dicts
 from address_parser.rnn import AddressRNN
 
 CHUNK_SIZE = 1000
-# BATCH_SIZE = 128
 BATCH_SIZE = 1000
 LSTM_DIM = 128
 LSTM_LAYERS = 2
 OUTPUT_DIM = len(ADDRESS_FIELD_CLASSES)
-SEQ_LENGTH = 100
+SEQ_LENGTH = 50
 LR = 0.001
 VOCAB = VOCAB_IDX_TO_CHAR.keys()
 CLIP = 5
-EPOCHS = 1
+EPOCHS = 3
+
+
+def _accuracy(out, y):
+    # Accuracy calculated based on the correct classification of each character in each address in the batch
+    class_preds = out.argmax(dim=1)
+    return (torch.sum(class_preds == y) / len(y)).item()
 
 
 def train(records):
+    """
+    :param records: list of address dict records from PAF.
+    TODO: Do pre-processing outside of this method and pass here pairs of encoded address and label arrays
+    """
     train_on_gpu = torch.cuda.is_available()
     model = AddressRNN(vocab=VOCAB, lstm_dim=LSTM_DIM, lstm_layers=LSTM_LAYERS,
                        output_dim=OUTPUT_DIM, seq_length=SEQ_LENGTH, train_on_gpu=train_on_gpu,
@@ -43,6 +52,7 @@ def train(records):
     for e in range(EPOCHS):
         batches = 0
         loss = None
+        accs = []
         for batch in chunks_from_iter(preprocess_addresses(records, seq_length=SEQ_LENGTH), BATCH_SIZE):
             X = torch.from_numpy(np.array([t[0] for t in batch]))
             y = torch.from_numpy(np.array([t[1] for t in batch]))
@@ -54,8 +64,9 @@ def train(records):
                 X = X.cuda()
                 y = y.cuda()
 
+            # Avoid backpropagating through the entire training history
             hidden = tuple([h.data for h in hidden])
-            model.zero_grad()
+            optimizer.zero_grad()
 
             out, hidden = model(X, hidden)
             """
@@ -83,9 +94,14 @@ def train(records):
             since the output has high scores for the correct class labels at the corresponding indexes and zero
             elsewhere.
             """
-            loss = criterion(out, y.reshape(BATCH_SIZE * SEQ_LENGTH))
-
+            y_reshaped = y.reshape(BATCH_SIZE * SEQ_LENGTH)
+            loss = criterion(out, y_reshaped)
             loss.backward()
+
+            acc = _accuracy(out, y_reshaped)
+            accs.append(acc)
+
+            # Clip gradients to avoid exploding gradients
             nn.utils.clip_grad_norm_(model.parameters(), CLIP)
             optimizer.step()
 
@@ -93,6 +109,7 @@ def train(records):
             if batches % 10 == 0:
                 print(f"Finished training on {batches} batches in epoch {e}")
                 print(f"Loss so far is {loss.item()}")
+                print(f"Average accuracy is {round(np.average(accs) * 100)}%")
         print(f"Finished training for epoch {e}")
         print(f"Loss at end of epoch {e} is {loss.item()}")
 
