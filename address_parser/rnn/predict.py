1928c36cd3a3f0ad35ffffa30550c20a50612722
Mohamed Abdelbary <mohamed.abdelbary@duedil.com>
2021-04-07 19:58:40 +0100

    simplify prediction script to expect file with address strings

--- address_parser/rnn/predict.py
+++ address_parser/rnn/predict.py
@@ -1,11 +1,8 @@
 import argparse
-
-import numpy as np
 import torch
 
-from address_parser.paf.preprocess import preprocess_addresses
-from address_parser.paf.util import chunks_from_iter, csv_records_to_dicts
-from address_parser.rnn.util import predict, address_components_from_pred
+from address_parser.paf.util import chunks_from_iter
+from address_parser.rnn.util import parse_raw_address
 
 CHUNK_SIZE = 1000
 
@@ -30,20 +27,13 @@ def main(test_file, model_path):
     all_records = []
     with open(test_file, "r") as f:
         for chunk in chunks_from_iter(f.readlines(), CHUNK_SIZE):
-            records = csv_records_to_dicts(chunk)[:100]
-            # TODO:
-            #  - The preprocessing method used here is the same as that used for training, which does some shuffling of components and extra
-            #  bits and pieces to make training harder. This needs to be replpaced with a method that just works with an address string and
-            #  just maps character in an arbitrary address string to the char index encoded version.
-            preprocessed = list(preprocess_addresses(records, model.seq_length))
-            addresses_encoded = torch.from_numpy(np.array([t[0] for t in preprocessed]))
-            preds = predict(addresses_encoded, model)
-            results.extend(address_components_from_pred(addresses_encoded, preds))
+            chunk_res = [parse_raw_address(add.strip(), model) for add in chunk]
+            results.extend(chunk_res)
             chunks += 1
             if chunks % 10 == 0:
                 print(f"Processed {chunks} chunks ({len(results)} records)")
             # Just for comparing results at the end
-            all_records.extend(records)
+            all_records.extend(chunk)
 
 
 if __name__ == "__main__":
